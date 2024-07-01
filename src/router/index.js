10edf3730e6bef313ba8cf7791ff4ac32e588917
zermoser <kittipoj.na@gmail.com>
2024-07-01 12:00:35 +0700

    Test Deploy is Complete !

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import HomeView from '../views/homeView.vue'
 import EditView from '../views/editView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/vue_todolist/'),
   routes: [
     {
       path: '/',
